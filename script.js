ce482625427a8cf6a0cdcdc36f7cb14c3536932d
Célelo <mariaceleste.lopez41@gmail.com>
2023-12-31 22:38:26 -0300

    incorporo resumen y totales por categorias terminado

--- script.js
+++ script.js
@@ -132,6 +132,9 @@ const renderCategories = (categories) => {
 
 const renderCategoriesOptions = (categories) => {
     cleanContainer("#categories")
+    $("#categories").innerHTML = `
+            <option value= "Todas">Todas</option>
+        `
     for (const category of categories) {
         $("#categories").innerHTML += `
             <option value= "${category.id}">${category.name}</option>
@@ -165,8 +168,10 @@ const infoForm = () => {
         type: $('#typeSelect').value,
         category: $('#inputCategories').value,
         date: $('#inputDate').value
-    };
+    }
 }
+
+// Add operation
 
 const addOperation = () => {
     const currentData = getData("operations")
@@ -208,6 +213,7 @@ const deleteDate = (operationId) => {
     setData('operations', currentData)
     window.location.reload()
 }
+// Balance
 
 const calculateBalance = (operationType) => {
     const currentData = getData("operations").filter(operation => operation.type === operationType)
@@ -216,6 +222,134 @@ const calculateBalance = (operationType) => {
         acc += operation.amount
     }
     return acc
+}
+
+const calculateTotalBalance = () =>  calculateBalance("Ganancia") - calculateBalance("Gasto")
+
+// Filters
+
+const filterByType = () => {
+    const value = $("#type").value
+    if (value != "Todos") {
+        const currentData = getData("operations").filter(operation => operation.type === value)
+        iterateOperations(currentData)
+    } else {
+        iterateOperations(allOperation)
+    }
+}
+
+const filterByCategory = () => {
+    const value = $("#categories").value
+    if (value != "Todas") {
+        const currentData = getData("operations").filter(operation => operation.category === value)
+        iterateOperations(currentData)
+    } else {
+        iterateOperations(allOperation)
+    }
+}
+
+const filterByDate = () => {
+    const value = $("#date").value
+    console.log(value)
+    const currentData = getData("operations").filter(operation => operation.date > value)
+    iterateOperations(currentData)
+}
+
+// // Sort 
+
+
+const sortBy = () => {
+    const value = $("#sortBy").value
+    if (value === "A/Z") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.description.toLowerCase() > b.description.toLowerCase()) {
+                return 1;
+            }
+            if (a.description.toLowerCase() < b.description.toLowerCase()) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    } 
+    if (value === "Z/A") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.description.toLowerCase() < b.description.toLowerCase()) {
+                return 1;
+            }
+            if (a.description.toLowerCase() > b.description.toLowerCase()) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    }
+    if (value === "Mayor monto") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.amount < b.amount) {
+                return 1;
+            }
+            if (a.amount > b.amount) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    }
+    if (value === "Menor monto") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.amount > b.amount) {
+                return 1;
+            }
+            if (a.amount < b.amount) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    }
+    if (value === "Más reciente") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.date < b.date) {
+                return 1;
+            }
+            if (a.date > b.date) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    }
+    if (value === "Menos reciente") {
+        const operations = getData("operations")
+        console.log(operations)
+        const currentData = operations.sort(function (a, b) {
+            if (a.date > b.date) {
+                return 1;
+            }
+            if (a.date < b.date) {
+                return -1;
+            }
+            return 0;
+        })
+        setData("operations", currentData)
+        iterateOperations(currentData)
+    }
 }
 
 // -------------------- CATEGORIES FUNCTIONS --------------------//
@@ -302,6 +436,8 @@ const initialize = () => {
     $("#incomeBalance").innerHTML = `+$${calculateBalance("Ganancia")}`
 
     $("#expensesBalance").innerHTML = `-$${calculateBalance("Gasto")}`
+
+    $("#totalBalance").innerHTML = `$${calculateTotalBalance()}`
 
     //----------------- LOGO EVENTS-----------------//
 
@@ -396,7 +532,21 @@ const initialize = () => {
         window.location.reload()
     })
 
+    $("#type").addEventListener("click" , () => {
+        filterByType()
+    })
 
+    $("#categories").addEventListener("click" , () => {
+        filterByCategory()
+    })
+
+    $("#date").addEventListener("change" , () => {
+        filterByDate()
+    })
+
+    $("#sortBy").addEventListener("change" , () => {
+        sortBy()
+    })
 
     //-----------------CATEGORIES SCREEN EVENTS-----------------//
 
@@ -423,3 +573,334 @@ const initialize = () => {
 }
 
 window.addEventListener('load', initialize())
+
+
+
+
+
+
+// funcion para obtener categorias y montos de operaciones almacenadas en el localStorage
+const getCategoriesAndAmounts = () => {
+    const updatedtData = getData('operations') || [];
+    const result = {};
+
+    for (const operation of updatedtData) {
+        const { category, amount } = operation;
+
+        // parseFloat : se utiliza para parsear una cadena de texto y convertirla a un número de punto flotante.Si la cadena contiene caracteres que no forman parte de un número válido, parseFloat detendrá el análisis y devolverá el valor numérico hasta ese punto
+        if (category in result) {
+            result[category] += parseFloat(amount);
+        } else {
+            result[category] = parseFloat(amount);
+        }
+    }
+    return result;
+};
+
+
+//si hay mas de dos elementos (operaciones) en el localStorage me devuelve true, si no , false. si hay mas de dos operaciones recien ahi se va aejecutar getCategoryWithMaxProfit
+const greaterThanTwoOperations = () => {
+    return (getData('operations') || []).length > 2;
+};
+
+
+// CATEGORIA CON MAYOR GANANCIA
+const getCategoryMax = () => {
+    if (!greaterThanTwoOperations()) return null;
+
+    // reutilizo la funcion getCategoriesAndAmounts() que me trae la categoria y el monto
+    const categoriesAndAmounts = getCategoriesAndAmounts();
+    console.log(categoriesAndAmounts)
+
+    let seniorCategory = null
+    let seniorAmount = 0;
+
+    for (const category in categoriesAndAmounts) {
+        // guardamos en una constante los montos obtenidos del localStorage
+        const localAmounts = categoriesAndAmounts[category];
+        console.log(localAmounts)
+
+        if (localAmounts > seniorAmount) {
+            seniorAmount = localAmounts, seniorCategory = category;
+        }
+    }
+
+    return {
+        category: seniorCategory ? getData("categories").find(cat => cat.id === seniorCategory)?.name : null,
+        amount: seniorAmount
+    };
+};
+
+
+const categoryMajorProfit = getCategoryMax();
+
+
+if (categoryMajorProfit) {
+    $('#tagCategory').textContent = `${categoryMajorProfit.category}`;
+    $('#amountCategory').textContent = `$+ ${categoryMajorProfit.amount}`;
+} else {
+    console.log("No hay suficientes operaciones para calcular la categoria con mayor ganancia.");
+}
+
+
+
+
+// Obtener categorías y montos de gasto
+const getCategoriesAndExpenseAmounts = () => {
+    const operations = getData('operations') || [];
+    const result = {};
+
+    for (const operation of operations) {
+        const { category, amount, type } = operation;
+
+        if (type === "Gasto") {
+            if (category in result) {
+                result[category] += amount;
+            } else {
+                result[category] = amount;
+            }
+        }
+    }
+
+    return result;
+};
+
+// CATEGORIA CON MAYOR GASTO
+const getCategoryMaxExpense = () => {
+    if (!greaterThanTwoOperations()) return null;
+
+    const categoriesAndAmounts = getCategoriesAndExpenseAmounts();
+
+    let maxExpenseCategory = null;
+    let maxExpenseAmount = 0;
+
+    for (const category in categoriesAndAmounts) {
+        const localAmount = categoriesAndAmounts[category];
+
+        if (localAmount > maxExpenseAmount) {
+            maxExpenseAmount = localAmount;
+            maxExpenseCategory = category;
+        }
+    }
+
+    return {
+        category: maxExpenseCategory ? getData("categories").find(cat => cat.id === maxExpenseCategory)?.name : null,
+        amount: maxExpenseAmount
+    };
+};
+
+const categoryMaxExpense = getCategoryMaxExpense();
+
+console.log("Categoría con mayor gasto:", categoryMaxExpense.category);
+console.log("Monto de gasto:", categoryMaxExpense.amount);
+$('#expenseCategory').textContent = `${categoryMaxExpense.category}`;
+$('#quantityMinorCategory').textContent = `$- ${categoryMaxExpense.amount}`;
+
+
+//  CATEGORIA CON MAYOR BALANCE
+
+
+// Obtener categorías y montos totales (considerando ganancias y gastos)
+const getCategoriesAndTotalAmounts = () => {
+    const operations = getData('operations') || [];
+    const result = {};
+
+    for (const operation of operations) {
+        const { category, amount, type } = operation;
+
+        if (category in result) {
+            result[category] += (type === 'Ganancia' ? amount : -amount);
+        } else {
+            result[category] = (type === 'Ganancia' ? amount : -amount);
+        }
+    }
+
+    return result;
+};
+
+// CATEGORIA CON MAYOR BALANCE
+const getCategoryMaxBalance = () => {
+    if (!greaterThanTwoOperations()) return null;
+
+    const categoriesAndAmounts = getCategoriesAndTotalAmounts();
+
+    let maxBalanceCategory = null;
+    let maxBalanceAmount = 0;
+
+    for (const category in categoriesAndAmounts) {
+        const localAmount = categoriesAndAmounts[category];
+
+        if (localAmount > maxBalanceAmount) {
+            maxBalanceAmount = localAmount;
+            maxBalanceCategory = category;
+        }
+    }
+
+    return {
+        category: maxBalanceCategory ? getData("categories").find(cat => cat.id === maxBalanceCategory)?.name : null,
+        amount: maxBalanceAmount
+    };
+};
+
+const categoryMaxBalance = getCategoryMaxBalance();
+
+console.log("Categoría con mayor balance:", categoryMaxBalance.category);
+console.log("Monto de balance:", categoryMaxBalance.amount);
+$('#balanceCategory').textContent = `${categoryMaxBalance.category}`;
+$('#balanceAmount').textContent = `$ ${categoryMaxBalance.amount}`;
+
+
+
+
+// MES CION MAYOR GANANCIA
+
+// funcion que me trae las fechas y monto
+const getDatesAndAmounts = (operationType) => {
+    const operations = getData('operations') || [];
+    const result = {};
+
+    for (const operation of operations) {
+        const { date, amount, type } = operation;
+
+        if (type === operationType) {
+            if (date in result) {
+                result[date] += amount;
+            } else {
+                result[date] = amount;
+                
+            }
+        }
+    }
+    return result;
+};
+
+
+// Obtener el mes con mayor ganancia
+const getMonthMaxProfit = () => {
+    const datesAndAmounts = getDatesAndAmounts('Ganancia');
+
+    let maxProfitMonth = null;
+    let maxProfitAmount = 0;
+
+    for (const date in datesAndAmounts) {
+        const localAmount = datesAndAmounts[date];
+
+        // de numero (12) lo convertimos al nombre del mes en español (diciembre)
+        const month = new Date(date).toLocaleString('es-ES', { month: 'long' });
+
+
+        if (localAmount > maxProfitAmount) {
+            maxProfitAmount = localAmount;
+            maxProfitMonth = month;
+        }
+    }
+
+    return {
+        month: maxProfitMonth,
+        amount: maxProfitAmount
+    };
+};
+
+const monthMaxProfit = getMonthMaxProfit();
+
+console.log("Mes con mayor ganancia:", monthMaxProfit.month);
+console.log("Monto de ganancia:", monthMaxProfit.amount);
+$('#monthCategoryProfit').textContent = `${monthMaxProfit.month}`;
+$('#monthAmountProfit').textContent = `${monthMaxProfit.amount}`;
+
+
+
+// CATEGORIA CON MAYOR GASTO 
+// Obtener fecha con mayor gasto
+const getDateWithMaxExpense = () => {
+    const datesAndAmounts = getDatesAndAmounts("Gasto");
+
+    let maxExpenseDate = null;
+    let maxExpenseAmount = 0;
+
+    for (const date in datesAndAmounts) {
+        const localAmount = datesAndAmounts[date];
+
+        const month = new Date(date).toLocaleString('es-ES', { month: 'long' });
+
+        if (localAmount > maxExpenseAmount) {
+            maxExpenseAmount = localAmount;
+            maxExpenseDate = month;
+        }
+    }
+
+    return {
+        date: maxExpenseDate,
+        amount: maxExpenseAmount
+    };
+};
+
+const dateWithMaxExpense = getDateWithMaxExpense();
+
+console.log("Fecha con mayor gasto:", dateWithMaxExpense.date);
+console.log("Monto de gasto:", dateWithMaxExpense.amount);
+$('#monthCategoryExpense').textContent = `${dateWithMaxExpense.date}`;
+$('#monthAmountExpense').textContent = `${dateWithMaxExpense.amount}`;
+
+
+
+
+
+// TOTALES POR CATEGORIA 
+
+const getTotalsByCategory = () => {
+    const operations = getData('operations') || [];
+    const totals = {};
+
+    // Obtener las primeras 4 operaciones
+    const firstFourOperations = operations.slice(0, 4);
+
+    for (const operation of firstFourOperations) {
+        const { category, amount, type } = operation;
+        const categoryName = getData("categories").find(cat => cat.id === category)?.name;
+
+        if (categoryName in totals) {
+            if (type === 'Ganancia') {
+                totals[categoryName].income += amount;
+            } else if (type === 'Gasto') {
+                totals[categoryName].expense += amount;
+            }
+        } else {
+            totals[categoryName] = {
+                income: type === 'Ganancia' ? amount : 0,
+                expense: type === 'Gasto' ? amount : 0
+            };
+        }
+    }
+
+    return totals;
+};
+
+
+const renderTotalsTable = () => {
+    const totalsByCategory = getTotalsByCategory();
+    const tableBody = $('#monthSummaryTable tbody');
+
+    // Limpiar el cuerpo de la tabla antes de renderizar los nuevos datos
+    tableBody.innerHTML = '';
+
+    for (const category in totalsByCategory) {
+        const { income, expense } = totalsByCategory[category];
+        const balance = income - expense;
+
+        // Crear una fila por cada categoría
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td class="pr-2">${category}</td>
+            <td class="pr-2">${income}</td>
+            <td class="pr-2">${expense}</td>
+            <td class="pr-2">${balance}</td>
+        `;
+
+        // Agregar la fila al cuerpo de la tabla
+        tableBody.appendChild(row);
+    }
+};
+
+// Llamar a la función para renderizar la tabla
+renderTotalsTable();
